8a2cb2f034d8fb1451684332c53585d3e4a8728d
zhiim <m3196253580@gmail.com>
2023-03-11 20:50:40 +0800

    add practice program from 3.5.1

---
+++ Chapter3/repeated_word_detection.cpp
@@ -0,0 +1,19 @@
+// 3.5.1 program
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int number_of_words = 0;
+	string previous = " ";
+	string current;
+	while (cin >> current)
+	{
+		++number_of_words;
+		if (previous == current)
+			cout << "word number " << number_of_words
+					<< " repeated word: " << current << '\n';
+		previous = current;
+	}
+}
